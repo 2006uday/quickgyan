3723253d9509d9427aeee3b27a6f5b9588c84328
2006uday <ak5884771@gmail.com>
2026-03-23 13:08:59 +0530

    with

--- backend2/index.js
+++ backend2/index.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 app.use(
     cors({
         origin: [process.env.FRONTEND_URL || "http://localhost:3000"],
-        credentials: true,
+        withcredentials: true,
     })
 );
 app.use(express.json());
